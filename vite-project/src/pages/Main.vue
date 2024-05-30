f0186c27f0392464b3f2fe2ff9e126658c77cce5
jusinnet01 <164996718+jusinnet01@users.noreply.github.com>
2024-05-30 17:49:33 +0800

    new book release design

--- vite-project/src/pages/Main.vue
+++ vite-project/src/pages/Main.vue
@@ -7,17 +7,16 @@
             <h1 class="marquee font-bold text-[#5988ff] font-mono whitespace-nowrap inline-block">
                 EBOOK STORE
             </h1>
-            <h1 class="marquee2 font-bold text-[#5988ff] font-mono whitespace-nowrap inline-block">
+            <h1 class="marquee font-bold text-[#5988ff] font-mono whitespace-nowrap inline-block">
                 &nbsp;EBOOK STORE
             </h1>
-            <h1 class="marquee3 font-bold text-[#5988ff] font-mono whitespace-nowrap inline-block">
+            <h1 class="marquee font-bold text-[#5988ff] font-mono whitespace-nowrap inline-block">
                 &nbsp;EBOOK STORE
             </h1>
 
 
         </div>
-        <div
-            class="col rounded-3xl m-3 max-w-[50%] flex items-center justify-center overflow-hidden">
+        <div class="col rounded-3xl m-3 max-w-[50%] flex items-center justify-center overflow-hidden">
             <div class="rounded-3xl max-h-[65vh] flex items-center justify-center overflow-hidden">
                 <video loop autoplay muted>
                     <source src="../assets/book.mp4" type="video/mp4">
@@ -26,11 +25,24 @@
         </div>
     </div>
     <!-- section2 -->
-    <div
-        class="flex outline outline-1 outline-[#16161a14] row mx-auto rounded-3xl m-3 min-h-[80vh] max-w-[95%] p-5">
-        <p class="block mb-0 ml-3 text-3xl font-extrabold text-blue-700 text-start block">New Book Releases</p>
-        <hr class="block mb-0 ml-0 w-full"/>
-         <summary class="card mb-3" style="border-color: transparent;background-color: transparent">
+    <div class="flex outline outline-1 outline-[#16161a14] row mx-auto rounded-3xl m-3 min-h-[80vh] max-w-[95%] p-5">
+        <p class="block mb-0 text-3xl font-extrabold text-blue-700 text-start">New Book Releases
+        </p>
+        <hr class="block mb-0 w-full" />
+        <div class="grid grid-cols-1 gap-8 mt-3 lg:grid-cols-4 md:grid-cols-3 xl:grid-cols-6 mx-auto">
+            <div v-for="book in books"
+                class="max-w-[226px] p-2 group hover:cursor-pointer hover:shadow-lg transition duration-500 ease hover:-translate-y-2">
+                <div class="group-hover:blur-[1px]">
+                    <img class="min-w-[210px] max-h-[315px]" :src="book.imgUrl" alt="">
+                </div>
+                <p class="text-lg font-bold mt-2 line-clamp-2">{{ book.title }}</p>
+                <p class="text-sm font-bold text-end mb-1">優惠價 NT$
+                    <span class="text-sm font-bold text-sky-600">{{ book.price }}</span>
+                </p>
+
+            </div>
+        </div>
+        <!-- <summary class="card mb-3" style="border-color: transparent;background-color: transparent">
             <div class="card-group row" id="card_group" style="display: grid;gap:12px;">
                 <div v-for="n in 10" class="col p-0 hover:shadow-lg transition duration-500 ease hover:-translate-y-2">
                     <div class="hover:border-indigo-600 hover:border-3 card w-100 h-100"
@@ -55,7 +67,7 @@
                     </div>
                 </div>
             </div>
-        </summary>
+        </summary> -->
     </div>
 
     <!-- section3 -->
@@ -63,34 +75,87 @@
 
     </div>
 </template>
-<script setup lang="ts">
+<script setup lang="ts" name="Main">
+import { ref } from 'vue'
+let books = ref([
+    {
+        title: "追尋失落的時光：一個旅人的心靈之旅",
+        imgUrl: "https://s3.amazonaws.com/virginia.webrand.com/virginia/50IgnOZ10MZ/cf9ca0abf2a77c311fc44d9f7ad1a99e/344/1648650462.png",
+        price: 100
+    },
+    {
+        title: "探索無盡的宇宙：星際航行的奇妙之旅",
+        imgUrl: "https://s3.amazonaws.com/virginia.webrand.com/virginia/TUjmSfBSBs2/3e59d5989f9eceaca8625b6bf780c2e4/344/1648665973.png",
+        price: 100
+    },
+    {
+        title: "破曉之前：夢幻世界的神秘啟示",
+        imgUrl: "https://s3.amazonaws.com/virginia.webrand.com/virginia/VGMSCiN4BsQ/0b08935b2765516d8c98167e6f47ab73/344/1648652486.png",
+        price: 100
+    },
+    {
+        title: "靈魂的奇跡：生命中的意義與使命",
+        imgUrl: "https://s3.amazonaws.com/virginia.webrand.com/virginia/yQev8MF16sZ/01e52a0edf3bf72541ecc41f0b41fc58/344/1648666817.png",
+        price: 100
+    },
+    {
+        title: "時光的記憶：遺忘與回憶的交錯",
+        imgUrl: "https://s3.amazonaws.com/virginia.webrand.com/virginia/nMzY6NJ97Xs/0d40507679ea0528a8cbc080bf0a74e6/344/1648657152.png",
+        price: 100
+    },
+    {
+        title: "破曉之前：夢幻世界的神秘啟示",
+        imgUrl: "https://s3.amazonaws.com/virginia.webrand.com/virginia/VGMSCiN4BsQ/0b08935b2765516d8c98167e6f47ab73/344/1648652486.png",
+        price: 100
+    },
+    {
+        title: "靈魂的奇跡：生命中的意義與使命",
+        imgUrl: "https://s3.amazonaws.com/virginia.webrand.com/virginia/yQev8MF16sZ/01e52a0edf3bf72541ecc41f0b41fc58/344/1648666817.png",
+        price: 100
+    },
+    {
+        title: "追尋失落的時光：一個旅人的心靈之旅",
+        imgUrl: "https://s3.amazonaws.com/virginia.webrand.com/virginia/50IgnOZ10MZ/cf9ca0abf2a77c311fc44d9f7ad1a99e/344/1648650462.png",
+        price: 100
+    },
+    {
+        title: "探索無盡的宇宙：星際航行的奇妙之旅",
+        imgUrl: "https://s3.amazonaws.com/virginia.webrand.com/virginia/TUjmSfBSBs2/3e59d5989f9eceaca8625b6bf780c2e4/344/1648665973.png",
+        price: 100
+    },
+    {
+        title: "破曉之前：夢幻世界的神秘啟示",
+        imgUrl: "https://s3.amazonaws.com/virginia.webrand.com/virginia/VGMSCiN4BsQ/0b08935b2765516d8c98167e6f47ab73/344/1648652486.png",
+        price: 100
+    },
+    {
+        title: "靈魂的奇跡：生命中的意義與使命",
+        imgUrl: "https://s3.amazonaws.com/virginia.webrand.com/virginia/yQev8MF16sZ/01e52a0edf3bf72541ecc41f0b41fc58/344/1648666817.png",
+        price: 100
+    },
+    {
+        title: "時光的記憶：遺忘與回憶的交錯",
+        imgUrl: "https://s3.amazonaws.com/virginia.webrand.com/virginia/nMzY6NJ97Xs/0d40507679ea0528a8cbc080bf0a74e6/344/1648657152.png",
+        price: 100
+    }
+])
+
 </script>
 <style scoped>
-.marquee,
-    .marquee2,
-    .marquee3 {
-        animation: slide 10s linear infinite;
-        font-size: 500px;
-        -webkit-text-stroke: 2px #5988ff;
-        color: transparent;
-    }
-
-    @keyframes slide {
-        0% {
-            transform: translatex(0)
-        }
-
-        100% {
-            transform: translatex(-100%)
-        }
-    }
-/* 卡片 */
-#card_group {
-    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+.marquee {
+    animation: slide 10s linear infinite;
+    font-size: 500px;
+    -webkit-text-stroke: 2px #5988ff;
+    color: transparent;
 }
-@include media-breakpoint-up(xxl) { 
-    #card_group {
-        grid-template-columns: repeat(4, minmax(200px, 1fr));
+
+@keyframes slide {
+    0% {
+        transform: translatex(0)
+    }
+
+    100% {
+        transform: translatex(-100%)
     }
 }
 </style>
