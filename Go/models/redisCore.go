a12750cc020d65090dabf20f7256ce51f0240541
lingjun0314 <jsweb3nft@gmail.com>
2024-08-14 09:12:01 +0800

    Revert "use failover redis"
    This reverts commit 7982bbdbf7468f7b42209fc689922d5a017b639a.

--- Go/models/redisCore.go
+++ Go/models/redisCore.go
@@ -4,9 +4,10 @@ import "github.com/redis/go-redis/v9"
 
 var RedisClient *redis.Client
 
-func init() {
-	RedisClient = redis.NewFailoverClient(&redis.FailoverOptions{
-		MasterName:    "mymaster",
-		SentinelAddrs: []string{"127.0.0.1:26379", "127.0.0.1:26380"},
+func init(){
+	RedisClient = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		Password: "",
+		DB: 0,
 	})
 }
