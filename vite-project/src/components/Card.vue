7100ad9466b0885d56b4cf8f5bf6651c347d66e8
jusinnet01 <164996718+jusinnet01@users.noreply.github.com>
2024-08-20 15:30:25 +0800

    card img

--- vite-project/src/components/Card.vue
+++ vite-project/src/components/Card.vue
@@ -12,9 +12,9 @@ defineProps<{ cardStore: Store[] , cardWidth2: string}>()
 </script>
 <template>
     <div v-for="book in cardStore" :class="cardWidth2"
-        class="max-w-[226px] min-w-[150px] p-2 group hover:cursor-pointer hover:shadow-lg transition duration-500 ease hover:-translate-y-2 snap-center">
+        class="max-w-[180px] min-w-[150px] p-2 group hover:cursor-pointer hover:shadow-lg transition duration-500 ease hover:-translate-y-2 snap-center">
         <div class="relative">
-            <img class="group-hover:blur-[3px] h-[230px] w-auto" :src="book.imgUrl">
+            <img class="group-hover:blur-[3px] h-[230px] w-auto mx-auto" :src="book.imgUrl">
             <div
                 class="hidden group-hover:block absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-blue-700 hover:border-0 rounded-full border-2 p-2 px-3 z-20 whitespace-nowrap">
                 <RouterLink target="_blank" :to="{path:'Product',query: { bookId: book.bookId }}" class="mb-0 text-sm font-semibold tracking-wide text-white">
